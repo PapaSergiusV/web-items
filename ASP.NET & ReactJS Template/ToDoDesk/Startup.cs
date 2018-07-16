fe95a353c267e328382420233870d4446f144208
Сергей Пономарев <31237530+PapaSergiusV@users.noreply.github.com>
2018-07-16 18:43:50 +0500

    v 1.0

---
+++ ASP.NET & ReactJS Template/ToDoDesk/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.SpaServices;
+using Microsoft.Extensions.Logging;
+
+namespace ToDoDesk
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(); // включаем поддержку webpack
+            }
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "DefaultApi",
+                    template: "api/{controller}/{action}");
+                routes.MapSpaFallbackRoute("spa-fallback", new { controller = "Home", action = "Index" }); // включаем поддержку клиентского роутинга
+            });
+        }
+    }
+}
